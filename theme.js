5933e9f07066dc49ab6b3aee6419ea8a741225a0
Tanmay Gejapati <tanmay.t.v.g+github@gmail.com>
2026-09-13 23:08:30 +0530

    Reduce casual email address scraping

--- theme.js
+++ theme.js
@@ -1,5 +1,14 @@
 const root = document.documentElement;
 const toggle = document.querySelector('.mode-toggle');
+
+const email = [116, 97, 110, 109, 97, 121, 46, 116, 46, 118, 46, 103, 64, 103, 109, 97, 105, 108, 46, 99, 111, 109]
+	.map(character => String.fromCharCode(character))
+	.join('');
+
+document.querySelectorAll('.email-link').forEach(link => {
+	link.href = `mailto:${email}`;
+});
+
 toggle.addEventListener('click', () => {
 	const dark = root.dataset.mode !== 'dark';
 	root.dataset.mode = dark ? 'dark' : 'light';
